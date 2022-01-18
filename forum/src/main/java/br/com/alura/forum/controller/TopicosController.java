a5f87f9bf447c4649cfd7b2ba5255a62a3504caa
leonardocosta <sir.costa@yahoo.com.br>
2022-01-17 21:06:31 -0300

    Consulta com filtros.

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,7 +1,6 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -20,8 +18,13 @@ public class TopicosController {
 
     @RequestMapping("/topicos")
     @ResponseBody
-    public List<TopicoDto> lista() {
-        List<Topico> topicos = topicoRepository.findAll();
-        return TopicoDto.converter(topicos);
+    public List<TopicoDto> lista(String nomeCurso) {
+        if(nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        }else{
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
     }
 }
